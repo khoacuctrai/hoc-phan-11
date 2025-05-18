81eca0e08a47ab770a02203678f089aa119d7c99
khoacuctrai <daonguyendangkhoa2022@gmail.com>
2025-05-18 20:35:45 +0700

    haha

--- home/urls.py
+++ home/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('like/<int:post_id>/', views.like_post, name='like_post'),
+    
 ]
